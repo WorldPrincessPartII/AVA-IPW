936d7a63a06746be1dbc37a7b8825e5e0cea0ac1
CFS20035603L\stk <steven.khoupongsy@uqconnect.edu.au>
2018-10-09 22:32:56 +1000

    Modal beginnings

---
+++ js/jayess.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    $("#download").click(function(){
+		$("#modal").toggleClass("is-active");
+	});
+	
+	$(".delete").click(function(){
+		$("#modal").toggleClass("is-active");
+	});
+});
